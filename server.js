9b7a07f8333e696b16b3680c1994b8ca64ce155a
sonam0296 <sonam43jha@gmail.com>
2023-07-07 19:33:36 +0530

    env args

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/create', async (req, res) => {
   const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
   const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
 
-  console.log('Test $$$$$$ test')
+  console.log('Test')
 
   await fs.writeFile(tempFilePath, content);
   exists(finalFilePath, async (exists) => {
@@ -45,4 +45,4 @@ app.post('/create', async (req, res) => {
   });
 });
 
-app.listen(80);
+app.listen(process.env.PORT);
